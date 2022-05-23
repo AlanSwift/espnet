dd3b0ffc65421316b35207aff7774d2478bfcce3
AlanSwift <shenkai200861@gmail.com>
2022-05-23 16:00:12 +0000

    fix

--- install.sh
+++ install.sh
@@ -38,9 +38,15 @@ conda install pytorch torchvision torchaudio cudatoolkit=11.1 -c pytorch
 
 
 
-ESPNET_ROOT=/home/v-shenkai/download/espnet/
+WORK_DIR=/home/v-shenkai/download/
+mkdir -p $WORK_DIR
+cd $WORK_DIR
+git clone https://github.com/AlanSwift/espnet.git
+cd espnet
 
-echo "if [ -z "${PS1:-}" ]; then
+echo "Generate anaconda activate file"
+echo -e "#!/usr/bin/env bash
+if [ -z "${PS1:-}" ]; then
     PS1=__dummy__
 fi
-. /home/v-shenkai/download/espnet/tools/anaconda/etc/profile.d/conda.sh && conda deactivate && conda activate espnet"
+. /home/v-shenkai/download/espnet/tools/anaconda/etc/profile.d/conda.sh && conda deactivate && conda activate espnet" > tools/activate_python.sh
