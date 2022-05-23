87ca1c25822de9dd3a39de739553e52a366abf3b
AlanSwift <shenkai200861@gmail.com>
2022-05-23 15:21:37 +0000

    fix

---
+++ egs2/wenetspeech/asr1/process.sh
@@ -0,0 +1,12 @@
+
+
+scp_path=/home/v-shenkai/wenet/dump/raw/train_L/wav.scp
+output_path=/blob/v-shenkai/data/wenet/inference/train/
+split_scps=""
+_nj=100
+
+for n in $(seq "${_nj}"); do
+    mkdir -p ${output_path}/${n}
+    split_scps+=" ${output_path}/${n}/wav.scp"
+done
+utils/split_scp.pl "${scp_path}" ${split_scps}
---
+++ install.sh
@@ -0,0 +1,46 @@
+# inference environment for P40 envorinment
+
+# install environment
+export LANG=en_US.UTF-8
+export LANGUAGE=
+export LC_CTYPE="en_US.UTF-8"
+export LC_NUMERIC=zh_CN.UTF-8
+export LC_TIME=zh_CN.UTF-8
+export LC_COLLATE="en_US.UTF-8"
+export LC_MONETARY=zh_CN.UTF-8
+export LC_MESSAGES="en_US.UTF-8"
+export LC_PAPER=zh_CN.UTF-8
+export LC_NAME=zh_CN.UTF-8
+export LC_ADDRESS=zh_CN.UTF-8
+export LC_TELEPHONE=zh_CN.UTF-8
+export LC_MEASUREMENT=zh_CN.UTF-8
+export LC_IDENTIFICATION=zh_CN.UTF-8
+export LC_ALL=
+sudo locale-gen en_US.UTF-8
+sudo locale-gen zh_CN.UTF-8
+
+sudo apt-get install cmake
+sudo apt-get install sox
+sudo apt-get install libsndfile1-dev
+sudo apt-get install ffmpeg
+sudo apt-get install flac
+
+
+# install espnet
+source /home/v-shenkai/.bashrc
+
+echo "create conda env: espnet"
+conda create -n espnet python=3.7 -y
+source activate espnet
+conda install pytorch torchvision torchaudio cudatoolkit=11.1 -c pytorch
+
+
+
+
+
+ESPNET_ROOT=/home/v-shenkai/download/espnet/
+
+echo "if [ -z "${PS1:-}" ]; then
+    PS1=__dummy__
+fi
+. /home/v-shenkai/download/espnet/tools/anaconda/etc/profile.d/conda.sh && conda deactivate && conda activate espnet"
